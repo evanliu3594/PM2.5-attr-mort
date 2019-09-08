1b1e7bbdb8bbf62ea10c537d478c267809f39729
evanliu3594 <55047262+evanliu3594@users.noreply.github.com>
2019-09-08 16:58:39 +0800

    1st release of my script for calculating PM2.5 attributed mortality griddedly based on IER model

---
+++ 2.RR计算.R
@@ -0,0 +1,155 @@
+setwd('D:\\201903人口健康效应')
+
+library(tidyverse);library(readxl);library(writexl)
+
+#############################################
+#             计 算 相 对 风 险             #
+#############################################
+
+RelativeRisk <- function(mode='t.test') {
+  IER_para<-read_excel('.\\Data\\IER_coefficients\\GBD2017参数.xlsx',sheet = 1)
+  
+  IER_para[is.na(IER_para)]<-'ALL'
+  
+  IER_curve<-function(x,alpha,beta,gamma,tmrel) {
+    if (x>tmrel) {
+      1 +  alpha * ( 1 - exp( -  beta * ( x -  tmrel ) ^ gamma))
+    }
+    else  1
+  }
+  
+  if (mode=='montecarlo') {
+    CI_mode<-function(data,n=50000,CI=0.95){
+      set.seed(seed)
+      df<-round(runif(n,min = 1,max = 1000),digits = 0)
+      df<-data[df]
+      if (max(df)==min(df)) {
+        data.frame(mean=unique(df),low=unique(df),up=unique(df))
+      } else {
+        t<-t.test(df,conf.level = CI)
+        data.frame(mean=t$estimate[1],low=t$conf.int[1],up=t$conf.int[2])
+      }
+    }
+  } else if (mode=='t.test') {
+      CI_mode<-function(x,CI=0.95){
+        if (max(x)==min(x)) {
+          data.frame(mean=unique(x),low=unique(x),up=unique(x))
+        } else {
+          t<-t.test(x,conf.level = CI)
+          data.frame(mean=t$estimate[1],low=t$conf.int[1],up=t$conf.int[2])
+        } 
+      }
+  } else  if (mode=='bootstrap') {
+        CI_mode<-function(data,times=1000,alpha=0.05){
+          x_bar<-mean(data)
+          x_sd<-sd(data)
+          n<-length(data)
+          M=matrix(rep(NA,times*n),nrow = times,ncol=n)
+          for(i in 1:times){
+            M[i,]<-sample(data,n,replace = T)
+          }
+          t_boot<-function(x){(mean(x)-x_bar)/sd(x)}
+          t_vector<-apply(M,1,t_boot)
+         
+          up_t_b<-sort(t_vector)[times*(1-alpha/2)] #求t_vector的分位点
+          down_t_b<-sort(t_vector)[times*alpha/2]
+          up_t<-x_bar+up_t_b*x_sd#置信上界
+          down_t<-x_bar+down_t_b*x_sd#置信下界
+          data.frame(mean=x_bar,low=down_t,up=up_t)
+        }
+  } else if (mode=='manual') {
+        CI_mode<-function(x,sigma=-1,alpha=0.05){
+          n<-length(x)
+          a<-mean(x)
+          if(sigma>=0){
+            b<-sigma/sqrt(n)*qnorm(1-alpha/2);df<-n
+          }
+          else{
+            b<-sd(x)/sqrt(n)*qt(1-alpha/2,n-1);df<- n-1
+          }
+          low=a-b;up=a+b
+          data.frame(mean=a,low=low,up=up)
+        }
+  }
+  
+  concentration<-round((seq(0,300,0.1)),digits = 1)
+  
+  RRlist<-data.frame(concentration)
+  
+  for (n in 0:32) {
+    from=n*1000+1; to=(n+1)*1000
+    data<-Reduce(merge,list(data.frame(IER_para[from:to,]),data.frame(concentration=concentration)))
+    result<-matrix(
+      with(data,mapply(IER_curve,alpha=alpha,beta=beta,gamma=gamma,tmrel=tmrel,x=concentration)),nrow = 3001,byrow = T)
+    RR_aggregate<-do.call(rbind,apply(result, 1 ,CI_mode))
+    division<-paste(unique(data$cause),unique(data$age),sep='_')
+    names(RR_aggregate)<-paste(division,c('mean','low','up'),sep = '_')
+    RRlist<-cbind(RRlist,RR_aggregate)
+  }
+  
+  idle<-data.frame(matrix(rep(1,30010),nrow = 3001,ncol = 10)) %>% `colnames<-`(c(paste0('IHD_',seq(0,20,5)),paste0('STROKE_',seq(0,20,5))))
+  
+  arrangement<-c("COPD_ALL","LC_ALL","LRI_ALL",
+                 "IHD_0","IHD_5","IHD_10","IHD_15","IHD_20","IHD_25","IHD_30","IHD_35","IHD_40","IHD_45","IHD_50",
+                 "IHD_55","IHD_60","IHD_65","IHD_70","IHD_75","IHD_80","IHD_85","IHD_90","IHD_95",
+                 "STROKE_0","STROKE_5","STROKE_10","STROKE_15","STROKE_20","STROKE_25","STROKE_30","STROKE_35","STROKE_40","STROKE_45","STROKE_50",
+                 "STROKE_55","STROKE_60","STROKE_65","STROKE_70","STROKE_75","STROKE_80","STROKE_85","STROKE_90","STROKE_95")
+  
+  RR_mean<-data.frame(`colnames<-`(idle,paste0(names(idle),'_mean')),concentration,select(RRlist,contains('mean')))
+  RR_mean<-RR_mean[c('concentration',paste0(arrangement,'_mean'))]
+  
+  RR_up<-data.frame(`colnames<-`(idle,paste0(names(idle),'_up')),concentration,select(RRlist,contains('up')))
+  RR_up<-RR_up[c('concentration',paste0(arrangement,'_up'))]
+  
+  RR_low<-data.frame(`colnames<-`(idle,paste0(names(idle),'_low')),concentration,select(RRlist,contains('low')))
+  RR_low<-RR_low[c('concentration',paste0(arrangement,'_low'))]
+  
+  RRlist<-list(mean=RR_mean,up=RR_up,low=RR_low)
+  
+  write_xlsx(RRlist,paste0(".\\Result\\归因系数\\GBD2017_RR_LYF_",mode,".xlsx"))
+  
+  return(RRlist)
+}
+
+
+#mode=(t.test       montecarlo     manual      bootstrap)
+
+RR<-RelativeRisk(mode = 't.test')
+
+
+############# D E V E L O P I N G ##############plotRR
+
+res<-as.data.frame(result)
+
+g<-ggplot(data = res)
+
+for (i in 1:1000) {
+  vi<-sym(paste0('V',i))
+  g<-g+geom_line(aes(x=concentration,y=!!vi))
+}
+g
+
+RR_mean<-read_excel(".\\Result\\归因系数\\GBD2017_RR_LYF.xlsx",sheet = 1)
+
+RR_up<-read_excel(".\\Result\\归因系数\\GBD2017_RR_LYF.xlsx",sheet = 2)
+
+RR_low<-read_excel(".\\Result\\归因系数\\GBD2017_RR_LYF.xlsx",sheet = 3)
+
+
+edpt<-NULL
+for (n in arrangement) {
+  df<-data.frame(concentration=round((seq(0,300,0.1)),digits = 1),
+                 select(RR_mean,contains(n)),
+                 select(RR_up,contains(n)),
+                 select(RR_low,contains(n)))
+  names(df)<-c('concentration','mean','up','low')
+  edpt[[n]]<-ggplot()+
+    geom_line(data=df,aes(x=concentration,y=mean))+
+    geom_line(data=df,aes(x=concentration,y=up))+
+    geom_line(data=df,aes(x=concentration,y=low))
+}
+
+print(endpt$LRI_ALL)
+
+
+############ test end
---
+++ 3.健康效应计算 v3.R
@@ -0,0 +1,175 @@
+########################################################################################
+#                                     计算归因死亡                                     #
+########################################################################################
+#                                                                                      #
+#  参数说明：                                                                          #
+#  RR：选择相对风险参数的置信上界（up）、均值（mean）和置信下界（low）                 #
+#  mode：计算RR置信区间的方式，有普通t检验（t.test），蒙特卡洛（montecarlo）及BOOTSTRAP#
+#  output：选择是否输出对应的结果，对应参数分别是：                                    #
+#     output_full=T（输出全网格全终端全年龄死亡人数）                                  #
+#     output_allage=T (输出全网格全终端死亡人数)                                       #
+#     output_allage_rign=T (输出各省份各终端死亡人数)                                  #
+#     output_nation=T (输出全国分终端死亡人数)                                         #
+#  本函数返回1个list，包含：                                                           #
+#  04-17年间全网格全终端全年龄死亡人数（full_grid）、                                  #
+#  04-17年间全网格全终端死亡人数（edpt_grid）、                                        #
+#  04-17年间各省份全终端死亡人数（edpt_rign）、                                        #
+#  以及04-17年间全国分终端死亡人数（nation）是dataframe                                #
+#                                                                                      #
+#  Created By YfLIU 20190904                                                           #
+#                                                                                      #
+########################################################################################
+
+setwd('D:\\201903人口健康效应')
+
+library(tidyverse);library(readxl);library(writexl)
+
+Attri_Mort<-function(RR='up',mode='montecarlo',output_full=F,output_allage=F,
+                     output_allage_rign=F,output_nation=F){
+  
+  #################################################################
+  #                     module1. 基础数据读取                     #
+  #################################################################
+  
+  FID_info<-read_csv('.//Data//FID信息表.txt')
+  Pop<-read_csv('.//Result//人口特征//网格人口.txt')
+  agroup<-read_excel('.//Data//04-17_人口结构&基准死亡率.xlsx',sheet = '年龄结构_GBD',range = cell_rows(24:44))
+  inci<-read_excel('.//Data//04-17_人口结构&基准死亡率.xlsx',sheet = '分年龄疾病发病率')
+  
+  RR_table<-read_excel(paste0(".//Result//归因系数//GBD2017_RR_LYF_",mode,".xlsx"),sheet = RR)
+  PM_raw<-read_csv('.//Result/PM2.5浓度//Annual_mean_clean.txt')
+  
+  #################################################################
+  #                     module2. 归因系数计算                     #
+  #################################################################
+  PAF_lookup<-data.frame(concentration=round(seq(0,300,0.1),digits = 1),(RR_table[,-1]-1)/RR_table[,-1])
+  
+  PAF<-NULL
+  
+  for (y in paste0(2004:2017)){
+    
+    PM<-select(PM_raw,FID,concentration=y)
+    PAF_grid<-left_join(PM,PAF_lookup,by = "concentration")
+    
+    PAF[[y]]$COPD<-bind_cols(PAF_grid[,1],replicate(20,select(PAF_grid,contains('COPD')),simplify = FALSE))
+    names(PAF[[y]]$COPD)<-c('FID',paste0('COPD_',seq(0,95,5),'_',RR))
+    
+    PAF[[y]]$LC<-bind_cols(PAF_grid[,1],replicate(20,select(PAF_grid,contains('LC')),simplify = FALSE))
+    names(PAF[[y]]$LC)<-c('FID',paste0('LC_',seq(0,95,5),'_',RR))
+    
+    PAF[[y]]$LRI<-bind_cols(PAF_grid[,1],replicate(20,select(PAF_grid,contains('LRI')),simplify = FALSE))
+    names(PAF[[y]]$LRI)<-c('FID',paste0('LRI_',seq(0,95,5),'_',RR))
+    
+    PAF[[y]]$IHD<-bind_cols(PAF_grid[,1],select(PAF_grid,contains('IHD')))
+    names(PAF[[y]]$IHD)<-c('FID',paste0('IHD_',seq(0,95,5),'_',RR))
+    
+    PAF[[y]]$Stroke<-bind_cols(PAF_grid[,1],select(PAF_grid,contains('Stroke')))
+    names(PAF[[y]]$Stroke)<-c('FID',paste0('STROKE_',seq(0,95,5),'_',RR))
+  }
+  
+  #################################################################
+  #                   module3. 全域过早死亡计算                   #
+  #################################################################
+  
+  full_grid<-NULL
+  for (y in paste0(2004:2017)){
+    PAF_annual<-PAF[[y]]
+    
+    mort<-NULL
+    for (edpt in unique(inci$endpoint)){
+      mort[[edpt]]<- PAF_annual[[edpt]][,-1] * as.data.frame( 
+        as.matrix(Pop[,y]) %*% t(as.matrix(agroup[,y])) %*%  diag(filter(inci,year == y & endpoint == edpt)[,-1:-2])) /1e5
+    }
+    full_grid[[y]]<-cbind(FID=Pop[,1],select(bind_cols(mort),-contains("LRI")),ALRI=mort[['LRI']][,1])
+  }
+  
+  if (output_full) {
+    write_xlsx(full_grid,paste0('.//Result//健康影响//Result_Full_',RR,'_',mode,'.xlsx'))
+  }
+  
+  #################################################################
+  #                module4. 省、国家级数据归纳计算                #
+  #################################################################
+  
+  allage_grid<-NULL
+  for (y in paste0(2004:2017)){
+    data<-full_grid[[y]]
+    df<-data.frame(FID=data[,1],
+                     COPD_SUM=rowSums(select(data,contains('COPD'))),
+                     IHD_SUM=rowSums(select(data,contains('IHD'))),
+                     LC_SUM=rowSums(select(data,contains('LC'))),
+                     ALRI_SUM=rowSums(select(data,contains('ALRI'))),
+                     Stroke_SUM=rowSums(select(data,contains('Stroke'))))
+    df$SUM_mort<-rowSums(df[,2:6])
+    allage_grid[[y]]<-df
+  }
+  
+  if (output_allage) {
+    write_xlsx(allage_grid,paste0('.//Result//健康影响//Result_allage_grid_',RR,'_',mode,'.xlsx'))
+  }
+  
+  allage_rign<-NULL
+  for (y in paste0(2004:2017)){
+    data<-allage_grid[[y]]
+    data<-left_join(data,FID_info,by='FID')
+    df<-data.frame(sum_ALL = tapply(data$SUM_mort, data$province, sum,na.rm=T),
+                   sum_COPD = tapply(data$COPD_SUM, data$province, sum,na.rm=T),
+                   sum_IHD = tapply(data$IHD_SUM, data$province, sum,na.rm=T),
+                   sum_LC = tapply(data$LC_SUM, data$province, sum,na.rm=T),
+                   sum_ALRI = tapply(data$ALRI_SUM, data$province, sum,na.rm=T),
+                   sum_Stroke = tapply(data$Stroke_SUM, data$province, sum,na.rm=T))
+    df<-cbind(province=rownames(df),df)
+    allage_rign[[y]]<-df
+  }
+  
+  if (output_allage_rign) {
+    write_xlsx(allage_rign,paste0('.//Result//健康影响//Result_allage_rign_',RR,'_',mode,'.xlsx'))
+  }
+  
+  nation<-NULL
+  for (y in paste0(2004:2017)){
+    data<-allage_rign[[y]]
+    nation<-rbind(nation,data.frame(year = y,
+                                    sum_ALL = sum(data$sum_ALL,na.rm = T),
+                                    sum_COPD = sum(data$sum_COPD,na.rm = T),
+                                    sum_IHD = sum(data$sum_IHD,na.rm = T),
+                                    sum_LC = sum(data$sum_LC,na.rm = T),
+                                    sum_ALRI = sum(data$sum_ALRI,na.rm = T),
+                                    sum_Stroke = sum(data$sum_Stroke,na.rm = T)))
+  }
+  
+  if (output_nation) {
+    write_xlsx(nation,paste0('.//Result//健康影响//Result_National_',RR,'_',mode,'.xlsx'))
+  }
+  
+  return(list(FULL=full_grid,ALLAGE=allage_grid,REGION=allage_rign,NATION=nation))
+  
+}
+
+#  RR='mean'    'up'   'low'
+
+#  mode='t.test'   'montecarlo'   'BOOTSTRAP'     'manual'
+
+#  output_full = T ,output_allage = T,output_allage_rign = T , output_nation = T
+
+mean<-Attri_Mort(RR='mean',mode = 'montecarlo', output_nation = T)
+
+up<-Attri_Mort(RR='up',mode = 'montecarlo', output_nation = T)
+
+low<-Attri_Mort(RR='low',mode = 'montecarlo', output_nation = T)
+
+
+
+###################计算2004-2017变化地理分布
+FULL_RESULT=mean$FULL
+
+variation<-data.frame(FID=FULL_RESULT[['2004']][,1],
+                      CHANGE=rowSums(FULL_RESULT[['2004']][,-1])-rowSums(FULL_RESULT[['2017']][,-1]))
+
+FID_info<-read_csv('.//Data//FID信息表.txt')
+
+variation<-left_join(variation,FID_info,by = 'FID')
+
+write_csv(variation,'.//Result//空间分布变化04-17.csv')
+
+
